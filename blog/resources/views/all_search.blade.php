0529b491bf7707441c9add1da3a9ba8deba935ae
kangsan-ks <to123dn@gmail.com>
2020-07-01 14:02:07 +0900

    2020-07-01 검색결과창 구현완료 all_search.blade.php

--- blog/resources/views/all_search.blade.php
+++ blog/resources/views/all_search.blade.php
@@ -1,3 +1,84 @@
 @include('header')
-
+<div id="sub_sec">
+    <div class="sub_top">
+        기술보호로부터 시작되는 <span class="bold">산업 경쟁력 강화</span>
+    </div>
+    <div class="sub_con">
+        <div class="sub_side">
+            <ul>
+                <li><a href="#none" class="bold f_nanum">검색결과</a></li>
+                <li><a href="/happycall01">검색결과</a></li>
+            </ul>
+        </div>
+        <div class="sub_outer">
+            <div class="sub_nav">
+                <div class="sub_subject f_nanum bold">
+                    검색결과
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li class="on">검색결과</li>
+                </ul>
+            </div>
+            <div class="sub_inner all_search">
+                <form action="">
+                    <div class="search_top_outer">
+                        <div class="search_top_inner">
+                            <h2><span>'보안교육'</span>에 대한 통합검색 결과입니다.</h2>
+                            <div class="search_value">
+                                <input type="text" value="보안교육">
+                                <button></button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <div class="search_result_box">
+                    <h3>정보마당 (2건)</h3>
+                    <div class="search_result">
+                        <p>안녕하세요 산업보안 정보도서관 <b>보안교육</b> 게시판입니다</p>
+                        <span>
+                            안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 
+                            정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다 ...
+                        </span>
+                    </div>
+                    <div class="search_result">
+                        <p>안녕하세요 산업보안 정보도서관 <b>보안교육</b> 게시판입니다</p>
+                        <span>
+                            안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 
+                            정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다 ...
+                        </span>
+                    </div>
+                </div>
+                <div class="search_result_box">
+                    <h3>정보마당 (2건)</h3>
+                    <div class="search_result">
+                        <p>안녕하세요 산업보안 정보도서관 <b>보안교육</b> 게시판입니다</p>
+                        <span>
+                            안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 
+                            정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다 ...
+                        </span>
+                    </div>
+                    <div class="search_result">
+                        <p>안녕하세요 산업보안 정보도서관 <b>보안교육</b> 게시판입니다</p>
+                        <span>
+                            안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 
+                            정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다안녕하세요 산업보안 정보도서관 게시판입니다 ...
+                        </span>
+                    </div>
+                </div>
+                <div class="pag_write search_list">
+                    <ul>
+                        <li><a href="#none"><img src="../img/pag_prev_btn.png" alt=""></a></li>
+                        <li class="on"><a href="#none">1</a></li>
+                        <li><a href="#none">2</a></li>
+                        <li><a href="#none">3</a></li>
+                        <li><a href="#none">4</a></li>
+                        <li><a href="#none">5</a></li>
+                        <li><a href="#none">6</a></li>
+                        <li><a href="#none"><img src="../img/pag_next_btn.png" alt=""></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 @include('footer')
