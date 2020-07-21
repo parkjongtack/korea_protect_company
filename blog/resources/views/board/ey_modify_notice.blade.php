6a2a351aa180ef5f8cdee271822805ea3d8bf131
parkjongtaek <lovedisket@naver.com>
2020-07-21 12:16:28 +0900

    산업보안 수정페이지 달력 걸기

--- blog/resources/views/board/ey_modify_notice.blade.php
+++ blog/resources/views/board/ey_modify_notice.blade.php
@@ -263,7 +263,7 @@
   <script type="text/javascript" src="/editor/js/third_party/spell_checker.min.js"></script>
   <script type="text/javascript" src="/editor/js/plugins/special_characters.min.js"></script>
   <script type="text/javascript" src="/editor/js/plugins/word_paste.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <!-- <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> -->
   <script>
     (function () {
       //new FroalaEditor("#edit")
@@ -434,10 +434,10 @@
 	}
 
   </script>
-<script
+<!-- <script
   src="https://code.jquery.com/jquery-3.5.1.min.js"
   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-  crossorigin="anonymous"></script>
+  crossorigin="anonymous"></script> -->
 <script>
     function fnCngList(sVal){
         var f = document.form1;
@@ -483,6 +483,29 @@
             $('<input type="file">').appendTo(".cate_file .line_content")
         }
     }
+</script>
+<script type="text/javascript">
+	$( function() {
+		 $("#start_period, #end_period").datepicker({
+			  showOn: "both", // 버튼과 텍스트 필드 모두 캘린더를 보여준다.
+			  changeMonth: true, // 월을 바꿀수 있는 셀렉트 박스를 표시한다.
+			  changeYear: true, // 년을 바꿀 수 있는 셀렉트 박스를 표시한다.
+			  minDate: '-100y', // 현재날짜로부터 100년이전까지 년을 표시한다.
+			  nextText: '다음 달', // next 아이콘의 툴팁.
+			  prevText: '이전 달', // prev 아이콘의 툴팁.
+			  numberOfMonths: [1,1], // 한번에 얼마나 많은 월을 표시할것인가. [2,3] 일 경우, 2(행) x 3(열) = 6개의 월을 표시한다.
+			  stepMonths: 3, // next, prev 버튼을 클릭했을때 얼마나 많은 월을 이동하여 표시하는가. 
+			  yearRange: 'c-100:c+10', // 년도 선택 셀렉트박스를 현재 년도에서 이전, 이후로 얼마의 범위를 표시할것인가.
+			  showButtonPanel: true, // 캘린더 하단에 버튼 패널을 표시한다. 
+			  currentText: '오늘 날짜' , // 오늘 날짜로 이동하는 버튼 패널
+			  closeText: '닫기',  // 닫기 버튼 패널
+			  dateFormat: "yy-mm-dd", // 텍스트 필드에 입력되는 날짜 형식.
+			  showAnim: "slide", //애니메이션을 적용한다.
+			  showMonthAfterYear: true , // 월, 년순의 셀렉트 박스를 년,월 순으로 바꿔준다. 
+			  dayNamesMin: ['월', '화', '수', '목', '금', '토', '일'], // 요일의 한글 형식.
+			  monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] // 월의 한글 형식.
+		 });
+	} );
 </script>
 {{-- 해피콜 --}}
 
